f74241c42e8745435961230b467fb6fb72cabad9
Hardok1 <hardokpl@gmail.com>
2020-02-05 01:40:19 +0100

    - Added controllers - Updated services and models

--- src/test/java/pl/edu/pwsztar/library/IntegrationTest.java
+++ src/test/java/pl/edu/pwsztar/library/IntegrationTest.java
@@ -30,6 +30,8 @@ public class IntegrationTest {
     public void controllersTest() throws Exception {
         mockMvc = standaloneSetup(accountController).build();
         Account admin = accountService.createMockAdmin();
+
+        //Creating account
         ResultActions resultActions = mockMvc.perform(
                 MockMvcRequestBuilders.post("/account/signup")
                         .accept(MediaType.APPLICATION_JSON)
@@ -37,16 +39,51 @@ public class IntegrationTest {
                         .content("{\"login\":\"AA\", \"password\":\"BB\", \"name\":\"CC\", \"lastName\":\"DD\", \"email\":\"EE\", \"place\":\"FF\", \"street\":\"GG\", \"homeNumber\":\"HH\", \"phoneNumber\":\"II\"}")
         );
         resultActions.andExpect(status().isCreated());
+
+
+        //Login to account
         resultActions = mockMvc.perform(
                 MockMvcRequestBuilders.post("/account/signin")
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\n" + "\t\"login\" : \"AA\",\n" + "\t\"password\" : \"BB\"\n" + "}")
         );
-       resultActions.andExpect(status().isOk());
-       resultActions.andExpect(content().json("\"login\": \"AA\",\n" +
-               "    \"password\": \"BB\",\n" +
-               "    \"name\": \"CC\""));
+        resultActions.andExpect(status().isOk());
+        resultActions.andExpect(content().json("{\"login\": \"AA\",\n" +
+                "    \"password\": \"BB\",\n" +
+                "    \"name\": \"CC\"}"));
+
+        //Add author
+        resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/authors/addAuthor")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "\t\"authorName\" : \"Andrzej Sapkowski\",\n" +
+                                "\t\"accountId\" : \"0\"\n" +
+                                "}")
+        );
+        resultActions.andExpect(status().isCreated());
+
+        //Add Book
+        resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.post("/authors/addAuthor")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\n" +
+                                "\t    \"name\": \"Harry Potter\",\n" +
+                                "        \"bookImageUrl\": \"adres url do miniaturki\",\n" +
+                                "        \"description\": \"opis książki\",\n" +
+                                "        \"price\": 20,\n" +
+                                "        \"authors\": [1],\n" +
+                                "        \"quantity\": 2,\n" +
+                                "\t    \"accountId\": 0\n" +
+                                "}")
+        );
+        resultActions.andExpect(status().isCreated());
+
+
     }
+
 
 }
